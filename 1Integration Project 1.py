a0449e13710857df3e1a5a8bb824d859f6cb01c3
cpspalding <54412573+cpspalding@users.noreply.github.com>
2019-09-17 12:20:04 -0400

    Update 1Integration Project 1.py

--- 1Integration Project 1.py
+++ 1Integration Project 1.py
@@ -2,8 +2,8 @@
 #My name is Charles Spalding but I prefer to be called Patrick.
 #This program is a basic estimator for Spalding Carpet Cleaners.
 print("Welcome to my basic estimating program for Spalding Carpet Cleaners!")
-carpet=input("How many dollars per a square foot:")
-feet=input("Insert how many square feet of carpet:")
+carpet=input("How many dollars per a square foot?:")
+feet=input("Insert how many square feet of carpet.:")
 num1=int(carpet)
 num2=int(feet)
 price=num1*num2
